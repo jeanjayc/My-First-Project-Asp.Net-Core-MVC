6e92f80c30a6c64dbaf021a12f97e1c4d452ca48
Jean Carlos <jeanmaster777@gmail.com>
2020-12-27 20:42:41 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web MVC App from C# Course";
+            ViewData["email"] = "jeanmaster777@gmail.com";
 
             return View();
         }
